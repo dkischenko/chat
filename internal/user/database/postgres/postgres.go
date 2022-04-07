c380036c8305cbf62f01756e87966c6340c88273
Dmitry Kischenko <dkischenko@users.noreply.github.com>
2022-04-07 18:58:20 +0300

    Added files for building app. (#14)

--- internal/user/database/postgres/postgres.go
+++ internal/user/database/postgres/postgres.go
@@ -30,7 +30,7 @@ func formatQuery(q string) string {
 
 func (db *postgres) StoreMessage(ctx context.Context, message *models.Message) (id int, err error) {
 	q := `
-		INSERT INTO messages(text, u_from, created_at)
+		INSERT INTO lets_go_chat.messages(text, u_from, created_at)
 		VALUES
 			($1, $2, $3)
 		RETURNING id
@@ -49,7 +49,7 @@ func (db *postgres) StoreMessage(ctx context.Context, message *models.Message) (
 func (db *postgres) GetUnreadMessagesCount(ctx context.Context, u *models.User) (count int, err error) {
 	q := `
 		SELECT count(id)
-		FROM messages
+		FROM lets_go_chat.messages
 		WHERE created_at > $1 AND u_from <> $2
 	`
 	row := db.pool.QueryRow(ctx, q, u.LastOnline, u.ID)
@@ -65,7 +65,7 @@ func (db *postgres) GetUnreadMessagesCount(ctx context.Context, u *models.User) 
 func (db *postgres) GetUnreadMessages(ctx context.Context, u *models.User, unreadMC int) (messages []models.Message, err error) {
 	q := `
 		SELECT id, text, u_from, created_at
-		FROM messages
+		FROM lets_go_chat.messages
 		WHERE created_at > $1 AND u_from <> $2
 	`
 	mRows, err := db.pool.Query(ctx, q, u.LastOnline, u.ID)
@@ -94,7 +94,7 @@ func (db *postgres) GetUnreadMessages(ctx context.Context, u *models.User, unrea
 
 func (db *postgres) Create(ctx context.Context, user *models.User) (id string, err error) {
 	q := `
-		INSERT INTO users(username, password_hash) 
+		INSERT INTO lets_go_chat.users(username, password_hash) 
 		    VALUES
 		           ($1, $2)
 		    RETURNING id
@@ -111,7 +111,7 @@ func (db *postgres) FindOneUser(ctx context.Context, username string) (u *models
 	u = &models.User{}
 	q := `
 		SELECT id, username, password_hash, key, is_online, last_online
-		FROM users WHERE username = $1
+		FROM lets_go_chat.users WHERE username = $1
 	`
 	row := db.pool.QueryRow(ctx, q, username)
 	err = row.Scan(&u.ID, &u.Username, &u.PasswordHash, &u.Key, &u.IsOnline, &u.LastOnline)
@@ -126,7 +126,7 @@ func (db *postgres) FindOneMessage(ctx context.Context, mid int) (m *models.Mess
 	m = &models.Message{}
 	q := `
 		SELECT id, text, u_from, created_at
-		FROM messages WHERE id = $1
+		FROM lets_go_chat.messages WHERE id = $1
 	`
 	row := db.pool.QueryRow(ctx, q, mid)
 	err = row.Scan(&m.ID, &m.Text, &m.UFrom, &m.CreatedAt)
@@ -142,7 +142,7 @@ func (db *postgres) FindByUUID(ctx context.Context, uuid string) (u *models.User
 	u = &models.User{}
 	q := `
 		SELECT id, username, password_hash, key, is_online, last_online
-		FROM users WHERE id = $1
+		FROM lets_go_chat.users WHERE id = $1
 	`
 	row := db.pool.QueryRow(ctx, q, uuid)
 	err = row.Scan(&u.ID, &u.Username, &u.PasswordHash, &u.Key, &u.IsOnline, &u.LastOnline)
@@ -156,7 +156,7 @@ func (db *postgres) FindByUUID(ctx context.Context, uuid string) (u *models.User
 
 func (db *postgres) UpdateKey(ctx context.Context, user *models.User, key string) (err error) {
 	q := `
-		UPDATE users
+		UPDATE lets_go_chat.users
 		SET key = $1
 		WHERE id = $2
 	`
@@ -170,13 +170,13 @@ func (db *postgres) UpdateKey(ctx context.Context, user *models.User, key string
 
 func (db *postgres) UpdateOnline(ctx context.Context, user *models.User, isOnline bool) (err error) {
 	qOnline := `
-		UPDATE users
+		UPDATE lets_go_chat.users
 		SET is_online = $1
 		WHERE id = $2
 	`
 
 	qOffline := `
-		UPDATE users
+		UPDATE lets_go_chat.users
 		SET is_online = $1, last_online = $2
 		WHERE id = $3
 	`
@@ -196,7 +196,7 @@ func (db *postgres) UpdateOnline(ctx context.Context, user *models.User, isOnlin
 func (db *postgres) GetOnline(ctx context.Context) (count int, err error) {
 	q := `
 		SELECT count(id) 
-		FROM users WHERE is_online = true
+		FROM lets_go_chat.users WHERE is_online = true
 	`
 	row := db.pool.QueryRow(ctx, q)
 	err = row.Scan(&count)
